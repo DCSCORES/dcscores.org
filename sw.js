b24e07d8ec712fd5102097b52fcd701830790910
woodylama <vaidaslamanauskas@gmail.com>
2018-02-08 03:46:51 +0200

    Add Service Worker

---
+++ sw.js
@@ -0,0 +1,62 @@
+var cacheName = 'dcscores:0001';
+var cacheFiles = [
+  '/',
+  '/impact/',
+  '/about-us/mission-and-history/',
+];
+
+
+
+
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName)
+    .then(function(cache) {
+      //console.log('Opened cache');
+      return cache.addAll(cacheFiles);
+    })
+  );
+});
+
+
+
+
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Grab the asset from SW cache.
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+    }).catch(function() {
+      // Can't access the network return an offline page from the cache
+      return caches.match('/offline/');
+    })
+  );
+});
+
+
+
+
+
+// Empty out any caches that don’t match the ones listed.
+self.addEventListener('activate', function(event) {
+
+  var cacheWhitelist = ['dcscores:0001'];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
